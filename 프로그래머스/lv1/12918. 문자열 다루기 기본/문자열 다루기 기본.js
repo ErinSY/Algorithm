c32da01adc6101fb677763666660c9bccb45f513
Seoyul Ha <seoyul0203@gmail.com>
2022-10-05 23:32:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,4 +1,3 @@
 function solution(s) {
-       return (s-0>=0 && s.length===4 && !s.includes('e')) || (s-0>=0 && s.length===6 && !s.includes('e'))?true:false
-return !s.includes('e')
+return (s-0>=0  && !s.includes('e')) && (s.length===6 ||s.length===4 )?true:false
 }
