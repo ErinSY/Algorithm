8d2ccd33398c7ef63ce0ecdfe66e5eabdb7b5bb6
Seoyul Ha <seoyul0203@gmail.com>
2022-11-03 20:32:55 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,5 +1,7 @@
 function solution(numbers, direction) {
    let arr=[];
-    direction==="right"?numbers.map((el,idx)=>idx===numbers.length-1?arr[0]=el:arr[idx+1]=el):numbers.map((el,idx)=>idx===0?arr[numbers.length-1]=el:arr[idx-1]=el)
+    direction==="right"?
+        numbers.map((el,idx)=>idx===numbers.length-1?arr[0]=el:arr[idx+1]=el):
+        numbers.map((el,idx)=>idx===0?arr[numbers.length-1]=el:arr[idx-1]=el)
     return arr;
 }
