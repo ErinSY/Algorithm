c48328566b51269278354dd0b4e45432dff9356a
Seoyul Ha <seoyul0203@gmail.com>
2022-11-04 16:24:17 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,5 @@
-function solution(hp) {
-    return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;
-}
+// function solution(hp) {
+//     return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;
+// }
+
+const solution = hp => parseInt(hp/5) + parseInt(hp%5/3)+(hp%5)%3
