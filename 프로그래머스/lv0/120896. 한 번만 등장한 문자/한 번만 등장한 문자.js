6b9c9a29bdf159a6096b16fe9d3232b6b8dd748f
Seoyul Ha <seoyul0203@gmail.com>
2022-11-18 08:54:54 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    let arr=[]
+    for (let i=0;i<=s.length-1;i++){
+        s.indexOf(s[i])===s.lastIndexOf(s[i])?arr.push(s[i]):''
+    }
+    return arr.sort().join('');
+}
