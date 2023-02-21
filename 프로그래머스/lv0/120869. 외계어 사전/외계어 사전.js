cdd0b80bfd5cc46002fe569822666dfc07962c5a
Seoyul Ha <seoyul0203@gmail.com>
2023-02-21 14:56:40 +0900

    [level 0] Title: 외계어 사전, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,12 +1,12 @@
 function solution(spell, dic) {
-    let result=[]
-    let answer = dic.map(el=>{
-            for(let i=0;i<spell.length;i++){
-                el.includes(spell[i])?result=1:result=2;
-                 if (result===2){break}
-                         }
-  return result
-}
-       )
-    return answer.includes(1)?1:2
+    let boolgroup=[]
+    for (i of dic){
+    let bool=[]
+        for(j of spell){
+            bool.push(i.includes(j))
+             }
+    boolgroup.push(bool)
+        }
+    let answer = boolgroup.map(el=>el.includes(false));
+    return answer.includes(false)?1:2
 }
