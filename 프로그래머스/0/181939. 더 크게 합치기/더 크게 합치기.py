eaeea4ed9371d1a3326b056cebb524cccc8f1cf9
Seoyul Ha <seoyul0203@gmail.com>
2024-10-31 08:57:50 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,7 +1,3 @@
 def solution(a, b):
-    answer = 0
-    if int(str(a)+str(b))<int(str(b)+str(a)):
-        answer = int(str(b)+str(a))
-    else:
-        answer = int(str(a)+str(b))
+    answer= max(int(str(a)+str(b)),int(str(b)+str(a)))
     return answer
