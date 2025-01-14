9aef6542d8f9d4b1b6781fe1a3ac9d44bb3c180d
Seoyul Ha <seoyul0203@gmail.com>
2025-01-14 13:28:00 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,19 @@
+def solution(str_list):
+    if "l" not in str_list and "r" not in str_list:
+        return []
+
+
+    if "l" in str_list and "r" in str_list:
+        if str_list.index("l") < str_list.index("r"):
+            return str_list[:str_list.index("l")]
+        else:
+            return str_list[str_list.index("r") + 1:]
+
+
+    if "l" in str_list:
+        return str_list[:str_list.index("l")]
+
+    if "r" in str_list:
+        return str_list[str_list.index("r") + 1:]
+
+    return []
